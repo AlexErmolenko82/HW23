e51f6fc1af8cc74655a24bf6490e8aaff355c580
Alex Ermolenko <alex.ermolenko1@gmail.com>
2022-05-16 23:12:45 +0300

    fix marks commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Title from "./components/Title";
 function App() {
   return (
     <div> 
-    <Title name = "Application regularStyle" type = "normal"/>
+    <Title name = "Application normalStyle" type = "normal"/>
     <Title name = "Application boldStyle" type = "bold"/>
     <Title name = "Application noStyle"/>
     </div>
   );
 }
-
 export default App;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,4 @@
-const Title = ({ name, type }) => {
-    const string = `Title name is - `;
-    if (type === "bold") {
-        return <p className = "bold">{string}{name}</p>
-    }
-    else {
-        if (type === "normal")
-            return <p className = "normal">{string}{name}</p>
-        else
-            return <p>{string}{name}</p>
-        }
+const Title = ({ name , type = "normal" }) => {
+    return <p style = {{fontWeight: type}}>Title name is - {name}</p>
 }
 export default Title;
